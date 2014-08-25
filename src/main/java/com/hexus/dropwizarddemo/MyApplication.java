b5c8654e303d92971c20f3d47d4995d3c14eb268
Mikhail Berastau <mikhail_berastau@epam.com>
2014-08-25 22:29:41 +0100

    Configuration added

--- src/main/java/com/hexus/dropwizarddemo/MyApplication.java
+++ src/main/java/com/hexus/dropwizarddemo/MyApplication.java
@@ -1,4 +1,22 @@
 package com.hexus.dropwizarddemo;
 
-public class MyApplication {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class MyApplication extends Application<MyApplicationConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new MyApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<MyApplicationConfiguration> myApplicationConfigurationBootstrap) {
+
+    }
+
+    @Override
+    public void run(MyApplicationConfiguration myApplicationConfiguration, Environment environment) throws Exception {
+
+    }
 }
--- src/main/java/com/hexus/dropwizarddemo/MyApplicationConfiguration.java
+++ src/main/java/com/hexus/dropwizarddemo/MyApplicationConfiguration.java
@@ -1,7 +1,32 @@
 package com.hexus.dropwizarddemo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class MyApplicationConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+    @NotEmpty
+    private String defaultName;
 
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
 }
